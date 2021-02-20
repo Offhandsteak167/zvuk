397ca81915621ca15423848c16fd8d5f14c05009
jwd2488 <jwd2488@rit.edu>
2021-02-20 16:50:33 -0500

    routes and new packages

--- src/app/routes.js
+++ src/app/routes.js
@@ -1,7 +1,23 @@
 module.exports = function(app) {
 
-    app.get('/',function(req,res){ // İSTEMCİNİN '/' İSTEĞİNE KARŞILIK VEREN KOD BLOĞU
-        res.render('index'); // INDEX VİEW DOSYASI RENDER EDİLDİ
+    app.get('/',function(req,res){
+        res.render('index');
+    });
+
+    app.get('/meeting',function(req,res){
+        res.render('meeting');
+    });
+    app.get('/queue',function(req,res){
+        res.render('call_queue');
+    });
+    app.get('/customer',function(req,res){
+        res.render('customer_login');
+    });
+    app.get('/business',function(req,res){
+        res.render('business_login');
+    });
+    app.get('/directory',function(req,res){
+        res.render('business_directory');
     });
 
 }
