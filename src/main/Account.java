31e64ac43e74555145e74383532118df6aec43ea
Patrick <bryant.swimmer@gmail.com>
2021-02-20 18:43:02 -0500

    Worked on account and Business

--- src/main/Account.java
+++ src/main/Account.java
@@ -1,3 +1,4 @@
+package main;
 public abstract class Account extends Thread {
     private int id;
     private String fname;
--- src/main/Interaction.java
+++ src/main/Interaction.java
@@ -1,3 +1,4 @@
+package main;
 public class Interaction {
     class Date{
         private int year;
