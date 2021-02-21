ea66b16218a32418e7641e468ff8c4167a8f9cab
Patrick <bryant.swimmer@gmail.com>
2021-02-21 08:39:06 -0500

    Fixed threading

--- src/main/server/CheckProcesses.java
+++ src/main/server/CheckProcesses.java
@@ -2,25 +2,32 @@ package main.server;
 
 public class CheckProcesses extends Thread{
     private CheckProcesses(){}
-    private Thread t;
-
+    private boolean isRunning;
     public void run(){
+        isRunning = true;
         update();
     }
     public void update(){
-        for (ProcessWrapper p:ServerSetup.processes) {
-            // kill all processes that have been running for more than 15 min
-            // very very very rough garbage collection
-            if(System.currentTimeMillis() - p.getTime() > 15 * 60 * 1000){
-                p.killProcess();
+        while(isRunning) {
+            for (ProcessWrapper p : ServerSetup.processes) {
+                // kill all processes that have been running for more than 15 min
+                // very very very rough garbage collection
+                if (System.currentTimeMillis() - p.getTime() > 15 * 60 * 1000) {
+                    p.killProcess();
+                }
+
+            }
+            try {
+                sleep(15000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                this.isRunning=false;
             }
         }
     }
 
     public void start() {
-        if (t == null) {
-            t = new Thread(this,"CheckProcesses");
-        }
+
     }
 
 
